3b98391ebe04c75b5edda9d54f441be52f1035cd
thiago-shibata <thiago.shibatta@gmail.com>
2024-12-03 10:08:01 -0300

    config: adding initial configs

---
+++ src/models/sqlite/settings/connection.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+
+class DBConnectionHandler:
+    def __init__(self):
+        self.__connection_string = "sqlite:///database.db"
+        self.__engine = None
+
+    def connect_to_db(self):
+        self.__engine = create_engine(self.__connection_string)
+
+    def get_engine(self):
+        return self.__engine
+
+db_connection_handler = DBConnectionHandler()
