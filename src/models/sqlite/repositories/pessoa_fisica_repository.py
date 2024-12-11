0e5d1b1790596359f8ed1e8b71b43991cd1b484a
thiago-shibata <thiago.shibatta@gmail.com>
2024-12-11 09:22:09 -0300

    feat: Adding insert_pessoa_fisica method in pessoa_fisica_repository

--- src/models/sqlite/repositories/pessoa_fisica_repository.py
+++ src/models/sqlite/repositories/pessoa_fisica_repository.py
@@ -6,6 +6,26 @@ class PessoaFisicaRepository():
     def __init__(self, db_connection: DBConnectionHandler) -> None:
         self.__db_connection = db_connection
 
+    def insert_pessoa_fisica(self, renda_mensal: float, idade: int, nome_completo: str, celular: str, email: str, categoria: str, saldo: float) -> None:
+        with self.__db_connection as database:
+            try:
+                pessoa_fisica_data = PessoaFisicaTable(
+                    renda_mensal=renda_mensal,
+                    idade = idade,
+                    nome_completo = nome_completo,
+                    celular = celular,
+                    email = email,
+                    categoria = categoria,
+                    saldo = saldo
+                )
+                database.session.add(pessoa_fisica_data)
+                database.session.commit()
+
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+
+
     def list_pessoa_fisica(self) -> list[PessoaFisicaTable]:
         with self.__db_connection as database:
             try:
